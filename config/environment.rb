3d06e115f2df34018304c84009ef715be9772a0f
william mathebula <mathebulawilliam@yahoo.com>
2012-02-13 15:22:30 +0200

    Initial commit

---
+++ config/environment.rb
@@ -0,0 +1,5 @@
+# Load the rails application
+require File.expand_path('../application', __FILE__)
+
+# Initialize the rails application
+MathebulaWM200970472IT00237P01::Application.initialize!
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MathebulaWM200970472IT00237P01::Application.config.secret_token = '5959318a5c7b9ca58b0508e0a2e9d0c7f8d34fad8d90b1c832ed04a560440888608754d45a18b82201a1d4b502638c43dd37797b2489d9a96f45f0a5a20d8199'
